3b07fe3e64ff4a098704c40d5c9c95294cb9e79b
Justin Nge <ping.nge@hotmail.com>
2020-11-06 18:18:53 +1100

    UPDATE: Added pesudocode for index and pacman

---
+++ app/Pacman.js
@@ -0,0 +1,10 @@
+// Create PacMac Class
+// Create game logic such as Place, Move, Facing
+
+/* Commands
+Place
+Move (Move one step forward)
+Left (Change facing direction 90deg to left)
+Right (Change facing direction 90deg to right)
+Report (Report current X, Y coordinate and current facing)
+*/
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // App entry point
-// Create PacMac Class
-// Create game logic such as Place, Move, Facing
 // Unit Testing
 // Refactor Code
 
 // Bonus
 // Create gui with Vue or React etc.
+
+// Main Game Class
+
+// Start the game
+// Accept user input for initial place command for X, Y and F coordinate.
+// Ask user for next command which can be Place, Move, Left, Right and Report.
+// Should print current coordinate after each command.
